cd7c78394cf3e84e5148b6c64f1753c99f56b122
Kumar Ankit <kumarankit.1511@gmail.com>
2023-09-13 22:22:28 +0530

    Added DFS

--- DFS.py
+++ DFS.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 graph = {}
 
@@ -35,56 +34,7 @@ add_edge("C","D")
 add_edge("E","D")
 print(graph)
 
-
-
-def DFS(node,visited,graph):
-    if node not in visited:
-        print(node)
-        visited.add(node)
-        for i in graph[node]:
-           DFS(i,visited,graph)
-
-visited = set()
-DFS("A",visited,graph)
-
-=======
-
-graph = {}
-
-def add_node(v):
-    if v in graph:
-        print(v," is already present")
-    else:
-       graph[v] = []
-
-
-def add_edge(v1,v2):
-     if v1 not in graph:
-       print(v1," is not present in graph")
-     elif v2 not in graph:
-         print(v2 ,"not present in graph")
-    
-     else:
-         graph[v1].append(v2)
-         graph[v2].append(v1)
-
-
-add_node("A")
-add_node("B")
-add_node("C")
-add_node("D")
-add_node("E")
-
-add_edge("A","B")
-add_edge("B","E")
-add_edge("A","C")
-add_edge("A","D")
-add_edge("B","D")
-add_edge("C","D")
-add_edge("E","D")
-print(graph)
-
-
+ 
 
 def DFS(node,visited,graph):
     if node not in visited:
@@ -96,4 +46,3 @@ def DFS(node,visited,graph):
 visited = set()
 DFS("A",visited,graph)
 
->>>>>>> f3064d0bf9487948467b12a2ab764075ddef1129
